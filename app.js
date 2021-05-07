bbd06ba6aaf009788e2c3358a2b06218f6b27541
v5Drozd <volodymyr.drozd.05@gmail.com>
2021-05-08 00:01:41 +0300

    first prototype

--- app.js
+++ app.js
@@ -30,57 +30,58 @@ weatherApp.config(function ($routeProvider) {
 weatherApp.service('weatherService', function () {
 
     
-    var NemirovskeShose = 
+    var chMistoNemirovskeShose = 
     {
-    "naeme": "Немирівське Шосе 84",
+    "name": "Немирівське Шосе 84",
     "lat":"49.22687458369613",
     "lon":"28.53870771897157"
     }
     
-    let IakovaShepeliaSt =
+    let chMistoIakovaShepeliaSt =
     {
     "name": "Якова Штепеля",
     "lat":"49.208530306399346",
     "lon":"22.849301837142939"
     }
 
-    let BarskeShose =
+    let chMistoBarskeShose =
     {
-    "name": "Якова Штепеля",
+    "name": "Барське Шосе",
     "lat":"49.23710269520618",
     "lon":"28.398134459453864"
     }
 
-    ChysteMisto = {NemirovskeShose,IakovaShepeliaSt,BarskeShose};
-
-
-
-    var self = this;
+    // var self = this;
     this.city = 'City'
-
-
+    this.chystiMista = [chMistoNemirovskeShose, chMistoIakovaShepeliaSt, chMistoBarskeShose];
+    this.searchingLocation = "";
 })
 
 weatherApp.controller('homeController', ['$scope', 'weatherService','$resource', function ($scope, weatherService, $resource) {
 
-    $scope.city = weatherService.city;
 
-    $scope.$watch('city', function () {
-        weatherService.city = $scope.city;
+     $scope.chystiMista = weatherService.chystiMista;
+
+    $scope.$watch('searchingLocation', function () {
+
+        weatherService.searchingLocation = $scope.searchingLocation;
 
     })
 }]);
 
 
 weatherApp.controller('forecastController', ['$scope', 'weatherService', '$resource', '$routeParams',function ($scope, weatherService, $resource, $routeParams) {
-    $scope.city = weatherService.city;
-
+  
+    $scope.locatoinName = weatherService.searchingLocation.name;
+    searvhingLat = weatherService.searchingLocation.lat;
+    searvhingLon = weatherService.searchingLocation.lon;
     $scope.days = $routeParams.days || 2;
 
     $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast",
     {callback: "JSON_CALLBACK"},  {get:{ method:"JSONP"}});
  
-    $scope.weatherResault = $scope.weatherAPI.get({q:$scope.city, appid:'6704185231a3fc5312b081a7659a853b'});
+    $scope.weatherResault = $scope.weatherAPI.get({lat:searvhingLat,lon:searvhingLon
+        , appid:'6704185231a3fc5312b081a7659a853b',units:'metric', lang:'ua' });
     console.log($scope.weatherResault);
 
 
@@ -95,3 +96,8 @@ weatherApp.controller('forecastController', ['$scope', 'weatherService', '$resou
          }  
 }])
 
+//Todo
+// ImortantJustWhetherIntoHouse)
+
+// give posibility to set and call any location, 
+// direction in time)
